290e887b1c1614fd263b72b7f1ae8c7085fe86ad
Divyank Goyal <goyal.divyank2012@outlook.com>
2026-05-09 16:05:10 +0530

    Delete NovaFix directory

--- NovaFix/scripts/wellness-music.js
+++
@@ -1,8 +0,0 @@
-(function(){
-  const frame = document.getElementById('wellnessMusicFrame');
-  if (!frame) return;
-  const src = 'https://open.spotify.com/embed/playlist/7JKDTbcB00R2kwBpLRo72r';
-  if (frame.getAttribute('src') !== src) {
-    frame.setAttribute('src', src);
-  }
-})();
